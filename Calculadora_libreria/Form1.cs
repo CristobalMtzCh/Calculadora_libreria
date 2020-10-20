6619bb88f6cc949915dbce328b45f7f25f7456b7
cristobal chavarria <cris_chavarriax98@outlook.com>
2020-10-20 13:25:10 -0700

    Practica

---
+++ Calculadora_libreria/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Prueba_Libreria;
+
+namespace Calculadora_libreria
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double suma = Operaciones.suma(5, 2);
+            MessageBox.Show(suma.ToString());
+        }
+    }
+}
